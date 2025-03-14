59725d392d2085d35e916d87b8bf116caf8ab18b
Stache <stache@stache.cat>
2025-03-14 18:52:00 +0100

    Graphical POC

--- webapp/main.py
+++ webapp/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object("config")
 
-@app.route('/')
-def home():
-    return render_template('template.html')
+@app.route("/")
+def main():
+    return render_template("main.html")
 
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     app.run(host=app.config["HOSTNAME"], port=app.config["PORT"])
